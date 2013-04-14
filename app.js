cb95ee7016eb59e81c4e78de028aba5905559097
Steve Perkins <stereosteve@gmail.com>
2013-04-14 09:23:14 -0400

    bring forward / send backward

--- app.js
+++ app.js
@@ -1,4 +1,45 @@
-$(function() {
-  $('.mouseable').draggable()
-  $('.mouseable').resizable()
-})
+var $body = $('body')
+var $menu = $('#menu')
+var $object = $('.object')
+var activeObject;
+
+$object.draggable().resizable()
+$menu.menu();
+
+$object.on('contextmenu', function(ev) {
+  if (ev.altKey || ev.ctrlKey || ev.metaKey) return true;
+  activeObject = $(ev.currentTarget);
+  $menu.show()
+  $menu.css({
+    top: ev.clientY + 'px',
+    left: ev.clientX + 'px',
+  });
+  return false;
+});
+
+$body.click(function() {
+  $menu.hide()
+});
+
+var ZMIN = 1;
+var ZMAX = 100;
+$('a').on('click', function(ev) {
+  var tar = $(ev.target);
+  var action = tar.data('action');
+  var z = activeObject.css('z-index');
+  activeObject.removeClass('atFront atBack');
+  if (action === 'bringForward') {
+    activeObject.css('z-index', z + 1);
+  }
+  if (action === 'bringToFront') {
+    $('.atFront').css('z-index', ZMAX).removeClass('atFront');
+    activeObject.css('z-index', ZMAX + 1).addClass('atFront');
+  }
+  if (action === 'sendBackward') {
+    activeObject.css('z-index', Math.max(z - 1, ZMIN));
+  }
+  if (action === 'sendToBack') {
+    $('.atBack').css('z-index', ZMIN).removeClass('atBack');
+    activeObject.css('z-index', ZMIN - 1).addClass('atBack');
+  }
+});
